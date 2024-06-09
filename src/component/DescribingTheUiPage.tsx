ef26e6cb102017ca348dbcbbcd88d8e3f3961280
yuko_banno <yuko_banno@mail.toyota.co.jp>
2024-06-09 14:27:38 +0900

    UI の記述 > コンポーネントに props を渡す > チャレンジ問題１

--- src/component/DescribingTheUiPage.tsx
+++ src/component/DescribingTheUiPage.tsx
@@ -1,16 +1,82 @@
 import "../css/DescribingTheUiPage.css";
 
-function Profile() {
-  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+// コンポーネントに props を渡す
+// チャレンジ問題
+// 1. コンポーネント抽出
+
+function getImageUrl(imageId: string, size = "s") {
+  return "https://i.imgur.com/" + imageId + size + ".jpg";
+}
+
+interface PersonProps {
+  name: string;
+  imageId: string;
+  profession: string;
+  awards: string[];
+  discovered: string;
+  imageSize?: number;
+}
+
+function Profile({
+  name,
+  imageId,
+  profession,
+  awards,
+  discovered,
+  imageSize = 70,
+}: PersonProps) {
+  return (
+    <section className="profile">
+      <h2>{name}</h2>
+      <img
+        className="avatar"
+        src={getImageUrl(imageId)}
+        alt={name}
+        width={imageSize}
+        height={imageSize}
+      />
+      <ul>
+        <li>
+          <b>Profession: </b>
+          {profession}
+        </li>
+        <li>
+          <b>Awards: {awards.length} </b>({awards.join(", ")})
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {discovered}
+        </li>
+      </ul>
+    </section>
+  );
 }
 
 export default function Gallery() {
   return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
+    <div>
+      <h1>Notable Scientists</h1>
+
+      <Profile
+        name="Maria Skłodowska-Curie"
+        imageId="szV5sdG"
+        profession="physicist and chemist"
+        awards={[
+          "Nobel Prize in Physics",
+          "Nobel Prize in Chemistry",
+          "Davy Medal",
+          "Matteucci Medal",
+        ]}
+        discovered="polonium (chemical element)"
+      />
+
+      <Profile
+        name="Katsuko Saruhashi"
+        imageId="YfeOqp2"
+        profession="geochemist"
+        awards={["Miyake Prize for geochemistry", "Tanaka Prize"]}
+        discovered="a method for measuring carbon dioxide in seawater"
+      />
+    </div>
   );
 }
