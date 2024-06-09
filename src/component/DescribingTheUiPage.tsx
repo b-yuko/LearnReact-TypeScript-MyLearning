b419f9994251f3368f22b61611b145df05c6d70e
yuko_banno <yuko_banno@mail.toyota.co.jp>
2024-06-09 14:39:42 +0900

    UI の記述 > コンポーネントに props を渡す > チャレンジ問題３

--- src/component/DescribingTheUiPage.tsx
+++ src/component/DescribingTheUiPage.tsx
@@ -2,65 +2,44 @@ import "../css/DescribingTheUiPage.css";
 
 // コンポーネントに props を渡す
 // チャレンジ問題
-// 2. props に基づく画像サイズ変更
+// 3. children に JSX を渡す
 
-interface Person {
-  name: string;
-  imageId: string;
+import { ReactNode } from "react";
+
+interface CardProps {
+  children: ReactNode;
+  title: string;
 }
 
-function getImageUrl(person: Person, size: string) {
-  return "https://i.imgur.com/" + person.imageId + size + ".jpg";
-}
-
-const ratio = window.devicePixelRatio;
-
-interface AvatarProps {
-  person: Person;
-  size: number;
-}
-
-function Avatar({ person, size }: AvatarProps) {
-  let thumbnailSize = "s";
-  if (size * ratio > 90) {
-    thumbnailSize = "b";
-  }
-
+function Card({ children, title }: CardProps) {
   return (
-    <img
-      className="avatar"
-      src={getImageUrl(person, thumbnailSize)}
-      alt={person.name}
-      width={size}
-      height={size}
-    />
+    <div className="card">
+      <div className="card-content">
+        <h1>{title}</h1>
+        {children}
+      </div>
+    </div>
   );
 }
 
 export default function Profile() {
   return (
-    <>
-      <Avatar
-        size={40}
-        person={{
-          name: "Gregorio Y. Zara",
-          imageId: "7vQD0fP",
-        }}
-      />
-      <Avatar
-        size={70}
-        person={{
-          name: "Gregorio Y. Zara",
-          imageId: "7vQD0fP",
-        }}
-      />
-      <Avatar
-        size={120}
-        person={{
-          name: "Gregorio Y. Zara",
-          imageId: "7vQD0fP",
-        }}
-      />
-    </>
+    <div>
+      <Card title="Photo">
+        <img
+          className="avatar"
+          src="https://i.imgur.com/OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={70}
+          height={70}
+        />
+      </Card>
+      <Card title="About">
+        <p>
+          Aklilu Lemma was a distinguished Ethiopian scientist who discovered a
+          natural treatment to schistosomiasis.
+        </p>
+      </Card>
+    </div>
   );
 }
