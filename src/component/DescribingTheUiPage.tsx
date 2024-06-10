f08e4ef2d4ab5b6d9c9a94a1fc886369f4bac8b4
yuko_banno <yuko_banno@mail.toyota.co.jp>
2024-06-10 16:25:56 +0900

    UI の記述 > リストのレンダー > チャレンジ問題４

--- src/component/DescribingTheUiPage.tsx
+++ src/component/DescribingTheUiPage.tsx
@@ -1,59 +1,27 @@
 import "../css/DescribingTheUiPage.css";
+import { Fragment } from "react";
 
 // リストのレンダー
 // チャレンジ問題
-// 3. リスト要素のコンポーネントを抽出
+// 4. セパレータ付きリスト
 
-interface Recipe {
-  id: string;
-  name: string;
-  ingredients: string[];
-}
+const poem = {
+  lines: [
+    "I write, erase, rewrite",
+    "Erase again, and then",
+    "A poppy blooms.",
+  ],
+};
 
-const recipes: Recipe[] = [
-  {
-    id: "greek-salad",
-    name: "Greek Salad",
-    ingredients: ["tomatoes", "cucumber", "onion", "olives", "feta"],
-  },
-  {
-    id: "hawaiian-pizza",
-    name: "Hawaiian Pizza",
-    ingredients: [
-      "pizza crust",
-      "pizza sauce",
-      "mozzarella",
-      "ham",
-      "pineapple",
-    ],
-  },
-  {
-    id: "hummus",
-    name: "Hummus",
-    ingredients: ["chickpeas", "olive oil", "garlic cloves", "lemon", "tahini"],
-  },
-];
-
-function Recipe({ name, ingredients }: Recipe) {
+export default function Poem() {
   return (
-    <div>
-      <h1>{name}</h1>
-      <ul>
-        {ingredients.map((ingredient) => (
-          <li key={ingredient}>{ingredient}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-export default function RecipeList() {
-  return (
-    <div>
-      <h1>Recipes</h1>
-      {recipes.map((recipe) => (
-        <Recipe key={recipe.id} {...recipe} />
+    <article>
+      {poem.lines.map((line, index) => (
+        <Fragment key={index}>
+          <p>{line}</p>
+          {poem.lines.length - 1 > index && <hr />}
+        </Fragment>
       ))}
-    </div>
+    </article>
   );
 }
