02774b61688f9cad61e3572bd904b2f8c12352e5
yuko_banno <yuko_banno@mail.toyota.co.jp>
2024-06-10 11:30:02 +0900

    UI の記述 > リストのレンダー > チャレンジ問題１

--- src/component/DescribingTheUiPage.tsx
+++ src/component/DescribingTheUiPage.tsx
@@ -1,44 +1,104 @@
 import "../css/DescribingTheUiPage.css";
 
-// 条件付きレンダー
+// リストのレンダー
 // チャレンジ問題
-// 3.連続する ? : を if と変数にリファクタ
+// 1. リストを 2 つに分割
 
-function Drink({ name }: { name: "tea" | "coffee" }) {
-  const drinks = {
-    tea: {
-      part: "leaf",
-      caffeine: "15–70 mg/cup",
-      age: "4,000+ years",
-    },
-    coffee: {
-      part: "bean",
-      caffeine: "80–185 mg/cup",
-      age: "1,000+ years",
-    },
-  };
-  const info = drinks[name];
+interface Person {
+  id: number;
+  name: string;
+  profession: string;
+  accomplishment: string;
+  imageId: string;
+}
 
+const people: Person[] = [
+  {
+    id: 0,
+    name: "Creola Katherine Johnson",
+    profession: "mathematician",
+    accomplishment: "spaceflight calculations",
+    imageId: "MK3eW3A",
+  },
+  {
+    id: 1,
+    name: "Mario José Molina-Pasquel Henríquez",
+    profession: "chemist",
+    accomplishment: "discovery of Arctic ozone hole",
+    imageId: "mynHUSa",
+  },
+  {
+    id: 2,
+    name: "Mohammad Abdus Salam",
+    profession: "physicist",
+    accomplishment: "electromagnetism theory",
+    imageId: "bE7W1ji",
+  },
+  {
+    id: 3,
+    name: "Percy Lavon Julian",
+    profession: "chemist",
+    accomplishment:
+      "pioneering cortisone drugs, steroids and birth control pills",
+    imageId: "IOjWm71",
+  },
+  {
+    id: 4,
+    name: "Subrahmanyan Chandrasekhar",
+    profession: "astrophysicist",
+    accomplishment: "white dwarf star mass calculations",
+    imageId: "lrWQx8l",
+  },
+];
+
+function getImageUrl(person: Person) {
+  return "https://i.imgur.com/" + person.imageId + "s.jpg";
+}
+
+interface ListSectionProps {
+  title: string;
+  people: Person[];
+}
+
+function ListSection({ title, people }: ListSectionProps) {
   return (
-    <section>
-      <h1>{name}</h1>
-      <dl>
-        <dt>Part of plant</dt>
-        <dd>{info.part}</dd>
-        <dt>Caffeine content</dt>
-        <dd>{info.caffeine}</dd>
-        <dt>Age</dt>
-        <dd>{info.age}</dd>
-      </dl>
-    </section>
+    <>
+      <h1>{title}</h1>
+      <ul>
+        {people.map((person) => (
+          <li key={person.id}>
+            <img src={getImageUrl(person)} alt={person.name} />
+            <p>
+              <b>{person.name}:</b>
+              {" " + person.profession + " "}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
-export default function DrinkList() {
+export default function List() {
+  const chemists: Person[] = [];
+  const everyoneElse: Person[] = [];
+  for (const person of people) {
+    if (person.profession === "chemist") {
+      chemists.push(person);
+    } else {
+      everyoneElse.push(person);
+    }
+  }
+
   return (
-    <div>
-      <Drink name="tea" />
-      <Drink name="coffee" />
-    </div>
+    <>
+      <article>
+        <ListSection title="Chemists" people={chemists} />
+      </article>
+      <article>
+        <ListSection title="Everyone Else" people={everyoneElse} />
+      </article>
+    </>
   );
 }
