e53e6b8ebb5d58c4abbd5dc3361f24d50ee8c54d
yuko_banno <yuko_banno@mail.toyota.co.jp>
2024-06-10 17:15:01 +0900

    UI の記述 > コンポーネントを純粋に保つ > チャレンジ問題１

--- src/component/DescribingTheUiPage.tsx
+++ src/component/DescribingTheUiPage.tsx
@@ -1,27 +1,38 @@
 import "../css/DescribingTheUiPage.css";
-import { Fragment } from "react";
 
-// リストのレンダー
+// コンポーネントを純粋に保つ
 // チャレンジ問題
-// 4. セパレータ付きリスト
+// 1. 壊れた時計を修理
 
-const poem = {
-  lines: [
-    "I write, erase, rewrite",
-    "Erase again, and then",
-    "A poppy blooms.",
-  ],
-};
+import { useState, useEffect } from "react";
 
-export default function Poem() {
+function useTime() {
+  const [time, setTime] = useState<Date>(() => new Date());
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+  return time;
+}
+
+export default function App() {
+  const time = useTime();
+  return <Clock time={time} />;
+}
+
+function Clock({ time }: { time: Date }) {
+  const hours = time.getHours();
+  let className;
+  if (hours >= 0 && hours <= 6) {
+    className = "night";
+  } else {
+    className = "day";
+  }
   return (
-    <article>
-      {poem.lines.map((line, index) => (
-        <Fragment key={index}>
-          <p>{line}</p>
-          {poem.lines.length - 1 > index && <hr />}
-        </Fragment>
-      ))}
-    </article>
+    <h1 id="time" className={className}>
+      {time.toLocaleTimeString()}
+    </h1>
   );
 }
