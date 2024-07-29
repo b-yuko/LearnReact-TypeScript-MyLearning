3ab9be27ffb700c92175f55e48362d0c63b7c313
b-yuko <yuko.banno.oo@gmail.com>
2024-07-29 19:34:51 +0900

    インタラクティビティの追加 > 一連の state の更新をキューに入れる > チャレンジ問題２

--- src/component/AddingInteractivityPage.tsx
+++ src/component/AddingInteractivityPage.tsx
@@ -1,33 +1,96 @@
 /*
   一連の state の更新をキューに入れる
   チャレンジ問題
-  1. リクエストカウンタの修正
+  2. state キューの独自実装
 */
 
-import { useState } from "react";
 
-export default function RequestTracker() {
-  const [pending, setPending] = useState(0);
-  const [completed, setCompleted] = useState(0);
+type StateUpdater = ((n: number) => number) | number;
 
-  async function handleClick() {
-    setPending((pending) => pending + 1);
-    await delay(3000);
-    setPending((pending) => pending - 1);
-    setCompleted((completed) => completed + 1);
-  }
+function increment(n: number): number {
+  return n + 1;
+}
+increment.toString = () => 'n => n+1';
 
+interface TestCaseProps {
+  baseState: number;
+  queue: StateUpdater[];
+  expected: number;
+}
+
+export default function App() {
   return (
     <>
-      <h3>Pending: {pending}</h3>
-      <h3>Completed: {completed}</h3>
-      <button onClick={handleClick}>Buy</button>
+      <TestCase
+        baseState={0}
+        queue={[1, 1, 1]}
+        expected={1}
+      />
+      <hr />
+      <TestCase
+        baseState={0}
+        queue={[
+          increment,
+          increment,
+          increment
+        ]}
+        expected={3}
+      />
+      <hr />
+      <TestCase
+        baseState={0}
+        queue={[
+          5,
+          increment,
+        ]}
+        expected={6}
+      />
+      <hr />
+      <TestCase
+        baseState={0}
+        queue={[
+          5,
+          increment,
+          42,
+        ]}
+        expected={42}
+      />
     </>
   );
 }
 
-function delay(ms: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+function TestCase({
+                    baseState,
+                    queue,
+                    expected
+                  }: TestCaseProps) {
+  const actual = getFinalState(baseState, queue);
+  return (
+    <>
+      <p>Base state: <b>{baseState}</b></p>
+      <p>Queue: <b>[{queue.join(', ')}]</b></p>
+      <p>Expected result: <b>{expected}</b></p>
+      <p style={{
+        color: actual === expected ? 'green' : 'red'
+      }}>
+        Your result: <b>{actual}</b>
+        {' '}
+        ({actual === expected ? 'correct' : 'wrong'})
+      </p>
+    </>
+  );
+}
+
+function getFinalState(baseState: number, queue: StateUpdater[]) {
+  let finalState = baseState;
+
+  for (const update of queue) {
+    if (typeof update === 'function') {
+      finalState = update(finalState);
+    } else {
+      finalState = update;
+    }
+  }
+
+  return finalState;
 }
